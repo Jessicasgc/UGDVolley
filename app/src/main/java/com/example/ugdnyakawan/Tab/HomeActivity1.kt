b39d1ed847593ebd98bc13ef58eefaf8e52b44cf
Justin <a>
2022-11-22 21:53:50 +0700

    fragment crud + profile - Justin

--- app/src/main/java/com/example/ugdnyakawan/Tab/HomeActivity1.kt
+++ app/src/main/java/com/example/ugdnyakawan/Tab/HomeActivity1.kt
@@ -1,23 +1,23 @@
 package com.example.ugdnyakawan.Tab
 
 import android.content.DialogInterface
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import com.example.ugdnyakawan.*
 import com.example.ugdnyakawan.Fragment.*
-import com.example.ugdnyakawan.Tab.*
+import com.example.ugdnyakawan.databinding.ActivityHome1Binding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlin.system.exitProcess
 
 class HomeActivity1 : AppCompatActivity() {
 
-    lateinit var Nav : BottomNavigationView
+    lateinit var binding: ActivityHome1Binding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home1)
+        binding = ActivityHome1Binding.inflate(layoutInflater)
+        setContentView(binding.root)
         supportActionBar?.hide()
 
         val fh = FragmentHome()
@@ -25,56 +25,51 @@ class HomeActivity1 : AppCompatActivity() {
         val fma = FragmentMakan()
         val fmi = FragmentMinum()
         val fpm = FragmentPemesanan()
-    Nav = findViewById(R.id.bottomNavigation)
 
-    Nav.setOnNavigationItemReselectedListener{
-        when (it.itemId) {
-            R.id.menu_home -> {
-                loadFragment(fh)
-                return@setOnNavigationItemReselectedListener
-            }
 
-            R.id.menu_profil -> {
-                loadFragment(fp)
-                return@setOnNavigationItemReselectedListener
-            }
+        loadFragment(fh)
+        binding.bottomNavigation.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.menu_home -> {
+                    loadFragment(fh)
+                }
 
-            R.id.menu_makanan -> {
-                loadFragment(fma)
-                return@setOnNavigationItemReselectedListener
-            }
-            R.id.menu_minuman -> {
-                loadFragment(fmi)
-                return@setOnNavigationItemReselectedListener
-            }
+                R.id.menu_profil -> {
+                    loadFragment(fp)
+                }
 
-            R.id.menu_pemesanan -> {
-                loadFragment(fpm)
-                return@setOnNavigationItemReselectedListener
-            }
+                R.id.menu_makanan -> {
+                    loadFragment(fma)
+                }
+                R.id.menu_minuman -> {
+                    loadFragment(fmi)
+                }
 
-            R.id.menu_exit -> {
-                val mBuilder = android.app.AlertDialog.Builder(this@HomeActivity1)
-                    .setTitle("Confirm")
-                    .setMessage("Are you sure you want to exit?")
-                    .setPositiveButton("Yes", object : DialogInterface.OnClickListener{
-                        override fun onClick(dialogInterface: DialogInterface, i: Int){
-                            exitProcess(0)
-                        }
-                    })
-                    .setNegativeButton("No", null)
-                    .show()
+                R.id.menu_pemesanan -> {
+                    loadFragment(fpm)
+                }
+
+                R.id.menu_exit -> {
+                    val mBuilder = android.app.AlertDialog.Builder(this@HomeActivity1)
+                        .setTitle("Confirm")
+                        .setMessage("Are you sure you want to exit?")
+                        .setPositiveButton("Yes", object : DialogInterface.OnClickListener {
+                            override fun onClick(dialogInterface: DialogInterface, i: Int) {
+                                exitProcess(0)
+                            }
+                        })
+                        .setNegativeButton("No", null)
+                        .show()
+                }
             }
+            true
+
         }
-        true
-
-    }
-}
-private  fun loadFragment(fragment: Fragment){
-    supportFragmentManager.beginTransaction().apply {
-        replace(R.id.container,fragment)
-        commit()
     }
 
-}
+    private fun loadFragment(fragment: Fragment) {
+        if (fragment != null) {
+            supportFragmentManager.beginTransaction().replace(R.id.container, fragment).commit()
+        }
+    }
 }
